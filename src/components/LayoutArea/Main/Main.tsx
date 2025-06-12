8dd5ab14a48233973b50a5a59301be253bb1bc17
orik <orikeni1309@gmail.com>
2025-06-12 17:09:20 +0300

    css main

--- src/components/LayoutArea/Main/Main.tsx
+++ src/components/LayoutArea/Main/Main.tsx
@@ -3,10 +3,11 @@ import './Main.css';
 import { useNavigate } from "react-router-dom";
 import { authStore } from "../../Auth/State/AuthState";
 import Dashboard from "../../DashBoard/DashBoard";
+import { FaTicketAlt } from "react-icons/fa";
 
 
 function Main(): JSX.Element {
-     const navigate = useNavigate();
+  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!authStore.getState().token);
 
   useEffect(() => {
@@ -18,18 +19,22 @@ function Main(): JSX.Element {
 
   
 
-  return (
+   return (
     <div className="Main">
-      {!isLoggedIn &&
-      <div className="not-logged-in">
-        <h2>You are not logged in</h2>
-        <p>Please log in to continue using the system.</p>
-        <button onClick={() => navigate("/login")}>Sign In</button>
-      </div> }
+      {!isLoggedIn && (
+        <div className="not-logged-in">
+            <div className="coupon-icon">
+                <i className="bi bi-ticket-perforated"></i>
+            </div>
+          <h1>Welcome to the Coupon Management System </h1>
+          <p>Discover, manage and purchase amazing coupons easily and securely.</p>
+          <p>Please log in to start your coupon experience.</p>
+          <button onClick={() => navigate("/login")}>Sign In</button>
+        </div>
+      )}
+
       <div className="welcome-card">
-      {isLoggedIn &&
-          <Dashboard/>
-      }
+        {isLoggedIn && <Dashboard />}
       </div>
     </div>
   );
